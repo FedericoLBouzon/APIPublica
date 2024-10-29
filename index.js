a541f4d04a333d90d235998e9adbe037b3670850
FedericoLBouzon <119512307+FedericoLBouzon@users.noreply.github.com>
2024-10-29 14:02:41 -0300

    AAAB

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json()); 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.get('/api/games', async (req, res) => {
     try {
